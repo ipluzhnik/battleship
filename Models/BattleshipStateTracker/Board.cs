56f1e3f991b7c2ad91488a954d4c8009a54677ef
Igor Pluzhnik <igore@jayride.com>
2018-12-10 14:41:26 +1100

    initial sceleton

---
+++ Models/BattleshipStateTracker/Board.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BattleshipStateTracker
+{
+    public class Board
+    {
+    }
+}
---
+++ Test/BattleshipStateTrackerTest/BoardTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleshipStateTrackerTest
+{
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
